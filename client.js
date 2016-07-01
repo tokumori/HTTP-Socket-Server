7568d70046c41e03500a7a6f02e2096aa2fcf256
Bryce <bsaito88@gmail.com>
2016-06-30 22:11:54 -1000

    Added connection and prints the response.

---
+++ client.js
@@ -0,0 +1,27 @@
+var net = require('net');
+
+var output = process.stdout;
+output.setEncoding('utf8');
+
+var args = process.argv;
+
+var URL = args[2];
+
+if (URL === undefined) {
+  console.log('Usage: node client.js [url] [GET/POST/PUT/DELETE/HEAD] [port]');
+  } else {
+    var client = net.createConnection(80, URL, function () {
+
+      var today = new Date();
+      var requestHeaders = 'GET / HTTP/1.1\n';
+      requestHeaders += 'Date: ' + today.toUTCString() + '\n';
+      requestHeaders += 'Host: ' + URL + '\n';
+      requestHeaders += 'User-Agent: http-client/0.1\n';
+      client.write(requestHeaders);
+      client.write('\n');
+  });
+
+  client.on('data', function (data) {
+    console.log(data.toString());
+  });
+}
