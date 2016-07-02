8bf0f2c14c1579034f0fae9bf64281a5f7a0af16
Bryce <bsaito88@gmail.com>
2016-07-02 10:42:05 -1000

    Added request data variable for request headers.

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var net = require('net');
+var fs = require('fs');
 
-var input = process.stdin;
-input.setEncoding('utf8');
 
 var PORT = 8080;
 var HOST = '127.0.0.1';
@@ -11,10 +10,12 @@ var server = net.createServer(function (client) {
 
   client.on('data', function (data) {
     var today = new Date();
+    var request = data.toString().split('\n');
+
     var statusCode = '200 OK';
     var response = 'HTTP/1.1' + statusCode + '\n';
     response += 'Date: ' + today.toUTCString() + '\n';
-    response += 'Server: nginy/1.0.0';
+    response += 'Server: nginy/1.0.0 \n';
     client.write(response);
   });
 
