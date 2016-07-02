652f7b2022f82da2d45c0644555af594dd6c3ee2
Bryce <bsaito88@gmail.com>
2016-07-02 13:43:32 -1000

    Added 404 feature for anything not hydrogen or helium.html.

--- server.js
+++ server.js
@@ -12,21 +12,28 @@ var server = net.createServer(function (client) {
     var today = new Date();
     var request = data.toString().split('\n');
     var path = request[0].split(' ')[1];
-
+    var statusCode = '200 OK';
     if (path === '/') {
       path = 'index.html';
-    } else {
+    } else if (path === '/helium.html' || path === '/hydrogen.html') {
       path = path.slice(1);
+    } else {
+      path = '404.html';
+      statusCode = '404 Not Found';
     }
 
-    var statusCode = '200 OK';
+
     var responseHeaders = 'HTTP/1.1 ' + statusCode + '\n';
     responseHeaders += 'Date: ' + today.toUTCString() + '\n';
     responseHeaders += 'Server: nginy/1.0.0 \n';
 
     var response = fs.readFile(path, 'utf-8', function (err, data) {
-      responseHeaders += 'Content-Length: ' + data.length + '\n';
-      client.write(responseHeaders + '\n' + data);
+      if (err) {
+        throw err;
+      } else {
+        responseHeaders += 'Content-Length: ' + data.length + '\n';
+        client.write(responseHeaders + '\n' + data);
+        }
       });
   });
 
