acd3d5a7e7e936daf269cc3349b664e16d6830ab
Bryce <bsaito88@gmail.com>
2016-07-02 11:52:23 -1000

    Got server working with curl --verbose.

--- server.js
+++ server.js
@@ -13,11 +13,21 @@ var server = net.createServer(function (client) {
     var request = data.toString().split('\n');
     var path = request[0].split(' ')[1];
 
+    if (path === '/') {
+      path = 'index.html';
+    } else {
+      path = path.slice(1);
+    }
+
     var statusCode = '200 OK';
-    var response = 'HTTP/1.1' + statusCode + '\n';
-    response += 'Date: ' + today.toUTCString() + '\n';
-    response += 'Server: nginy/1.0.0 \n';
-    client.write(response);
+    var responseHeaders = 'HTTP/1.1 ' + statusCode + '\n';
+    responseHeaders += 'Date: ' + today.toUTCString() + '\n';
+    responseHeaders += 'Server: nginy/1.0.0 \n';
+
+    var response = fs.readFile(path, 'utf-8', function (err, data) {
+      responseHeaders += 'Content-Length: ' + data.length + '\n';
+      client.write(responseHeaders + '\n' + data);
+      });
   });
 
 
