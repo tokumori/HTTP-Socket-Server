a656191e84adac171010ac077482afa40d6b39c3
Bryce <bsaito88@gmail.com>
2016-07-02 14:23:41 -1000

    Added CLI options for method header and port.

--- client.js
+++ client.js
@@ -7,24 +7,47 @@ var args = process.argv;
 
 var reqParts = args[2].split('/');
 var URL = reqParts[0];
-var path = '/' + reqParts[1];
+var path;
+if (path === undefined) {
+  path = '/';
+  } else {
+  path = '/' + reqParts[1];
+}
 var PORT;
 if (URL === 'localhost' || URL === '127.0.0.1') {
   PORT = 8080;
 } else {
   PORT = 80;
 }
+var method = 'GET';
+var headers = false;
+
+if(args[3]) {
+  for (var i = 3; i < args.length; i++) {
+    if (args[i] === '--method') {
+      method = args[i + 1].toUpperCase();
+    }
+    if (args[i] === '--header') {
+      headers = true;
+    }
+    if (args[i] === '--port') {
+      PORT = args[i + 1];
+    }
+  }
+}
+console.log(PORT);
 
 if (URL === undefined) {
-  console.log('Usage: node client.js [url] [GET/POST/PUT/DELETE/HEAD] [port]');
+  console.log('Usage: node client.js [url] --method [GET/POST/PUT/DELETE/HEAD] --header --port [port number]');
   } else {
     var client = net.createConnection(PORT, URL, function () {
 
       var today = new Date();
-      var requestHeaders = 'GET ' + path + ' HTTP/1.1\n';
+      var requestHeaders = method + ' ' + path + ' HTTP/1.1\n';
       requestHeaders += 'Date: ' + today.toUTCString() + '\n';
       requestHeaders += 'Host: ' + URL + '\n';
       requestHeaders += 'User-Agent: http-client/0.1\n';
+      console.log(requestHeaders);
       client.write(requestHeaders + '\n');
   });
 
